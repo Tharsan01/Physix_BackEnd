e0125d7712767f62f70df8297f9fdc3a745344f9
T.Tharsan <162117048+Tharsan01@users.noreply.github.com>
2025-06-19 14:47:04 +0530

    updated

--- services/inquiryService.js
+++ services/inquiryService.js
@@ -1,37 +1,37 @@
 const inquiryRepository = require('../repository/inquiryRepository');
 const InquiryDTO = require('../dtos/inquiryDTO');
 
-async function createInquiry(userId, subject, message) {
+const createInquiry = async (userId, subject, message) => {
   const createdInquiry = await inquiryRepository.create({ userId, subject, message });
   return new InquiryDTO(createdInquiry);
-}
+};
 
-// async function replyToInquiry(inquiryNumber, reply) {
-//   const inquiry = await inquiryRepository.findByInquiryNumber(inquiryNumber);
-//   if (!inquiry) throw new Error('Inquiry not found');
-//   inquiry.reply = reply;
-//   const updatedInquiry = await inquiryRepository.save(inquiry);
-//   return new InquiryDTO(updatedInquiry);
-// }
+// Version using ID for reply
+const replyToInquiry = async (inquiryNumber, reply) => {
+  if (!inquiryNumber || !reply) {
+    throw new Error('Inquiry number and reply are required');
+  }
 
-async function replyToInquiry(id, reply) {
-  const inquiry = await inquiryRepository.findById(id);
+  const inquiry = await inquiryRepository.findByInquiryNumber(inquiryNumber);
   if (!inquiry) throw new Error('Inquiry not found');
+
   inquiry.reply = reply;
   const updatedInquiry = await inquiryRepository.save(inquiry);
-  return new InquiryDTO(updatedInquiry);
-}
 
-async function getAllInquiries() {
+  return new InquiryDTO(updatedInquiry);
+};
+
+
+const getAllInquiries = async () => {
   const inquiries = await inquiryRepository.findAll();
   return inquiries.map(i => new InquiryDTO(i));
-}
+};
 
-async function getInquiryByNumber(inquiryNumber) {
+const getInquiryByNumber = async (inquiryNumber) => {
   const inquiry = await inquiryRepository.findByInquiryNumber(inquiryNumber);
   if (!inquiry) throw new Error('Inquiry not found');
   return new InquiryDTO(inquiry);
-}
+};
 
 module.exports = {
   createInquiry,
