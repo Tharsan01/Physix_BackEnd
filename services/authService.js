2de7ebe237ae3a17d76091a4a68b03faa129131f
Dasun Sathsara <130266928+Dasun110@users.noreply.github.com>
2025-07-30 16:14:41 +0530

    Update authService.js

--- services/authService.js
+++ services/authService.js
@@ -11,6 +11,7 @@ const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString()
 const sendOTPEmail = async (email, otp) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
